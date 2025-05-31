40c26cc530f9ded69f7ac5ad6414693e1061b4a6
hashinodithya <bandara-se19019@stu.kln.ac.lk>
2025-06-01 00:54:04 +0530

    add menu icons

--- app/views/driver/(tabs)/_layout.tsx
+++ app/views/driver/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons, MaterialIcons, FontAwesome5 } from '@expo/vector-icons'
 
 const _layout = () => {
   return (
@@ -8,19 +8,32 @@ const _layout = () => {
         <Tabs.Screen name="home" options={{
             title: 'Home',
             headerShown: false,
-        }} />
-        <Tabs.Screen name="account" options={{
-            title: 'Account',
-            headerShown: false,
-        }} />
-        <Tabs.Screen name="settings" options={{
-            title: 'Settings',
-            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+            ),
         }} />
         <Tabs.Screen name="tutorial" options={{
             title: 'Tutorial',
             headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+                <MaterialIcons name="menu-book" color={color} size={size} />
+            ),
         }} />
+        <Tabs.Screen name="account" options={{
+            title: 'Account',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+                <FontAwesome5 name="file-alt" color={color} size={size} />
+            ),
+        }} />
+        <Tabs.Screen name="settings" options={{
+            title: 'Settings',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons name="settings" color={color} size={size} />
+            ),
+        }} />
+        
     </Tabs>
   )
 }
